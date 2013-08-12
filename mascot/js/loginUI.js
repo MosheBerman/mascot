266ae8b5ff5530ed91dc9d62ec16d3c00b601ce2
Moshe Berman <moshberm@gmail.com>
2013-08-12 16:16:10 -0400

    Commit latest login files.

--- mascot/js/loginUI.js
+++ mascot/js/loginUI.js
@@ -1,2 +1,13 @@
-console.log("Loaded the landing UI module."); 	
 
+
+function replaceBannerWithNewOne() 
+{
+	var newBannerURL = "chrome-extension://" + extensionID + "/img/studentsfirst-banner.png";
+
+	document.getElementById("banner").getElementsByTagName("img")[0].src = newBannerURL;
+}
+
+function loginMain()
+{
+	replaceBannerWithNewOne();
+}
