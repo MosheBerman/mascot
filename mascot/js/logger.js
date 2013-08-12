45dab1693a6f3c583dbe8bc1c61e46c082e0229f
Moshe Berman <moshberm@gmail.com>
2013-08-12 16:13:18 -0400

    Added logger js file.

---
+++ mascot/js/logger.js
@@ -0,0 +1,10 @@
+/*
+ *	Created by Moshe Berman on August 12, 2013
+ *
+ *	Logger.js simply logs messages to the console
+ *	but may do other things in the future.
+ */
+
+function log(message) {
+	consoler.log(message);
+}
